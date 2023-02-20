6e38364fdb626768a3ff2170e0c65df8735519b7
Hari Seldon <cryptoseldon33@proton.me>
2023-02-20 20:41:28 +0530

    verifier wrapper

--- verifier/src/lib.rs
+++ verifier/src/lib.rs
@@ -6,6 +6,24 @@ use vm_core::{utils::collections::Vec, ProgramOutputs};
 use winterfell::VerifierError;
 use bytemuck::{Pod, Zeroable};
 
+//use rescue::rp64::Element;
+//use rescue::rp64::params::MDS;
+//use rescue::rescue::{RescueHashParams, StatefulRescueHash};
+
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    program_error::ProgramError,
+    pubkey::Pubkey,
+};
+
+
+//use rust_crypto::util::hex;
+
+
 
 // EXPORTS
 // ================================================================================================
@@ -13,6 +31,10 @@ use bytemuck::{Pod, Zeroable};
 pub use assembly;
 pub use vm_core::chiplets::hasher::Digest;
 pub use winterfell::StarkProof;
+
+use bytemuck;
+
+
 
 // VERIFIER
 // ================================================================================================
@@ -34,8 +56,73 @@ pub use winterfell::StarkProof;
 ///
 /// # Errors
 /// Returns an error if the provided proof does not prove a correct execution of the program.
-pub mod entrypoint;
-pub mod processor;
+
+// Declare and export the program's entrypoint
+entrypoint!(process_instruction("c8653f31a1098e1b83c5d4972ec544cac00aa784bba18b5a9db7478977d38e68"));
+
+// Program entrypoint's implementation
+//#[derive(Copy, Clone, Pod, Zeroable)]
+//#[repr(C)]
+pub struct StkProof{
+    pub proof: StarkProof,
+}
+
+pub fn process_instruction(
+    program_id: &Pubkey, // Public key of the account the hello world program was loaded into
+    accounts: &[AccountInfo], // The account storing the Proof Data
+    program_hash: Digest,
+    _instruction_data: &[u8],// String
+) -> ProgramResult {
+    msg!("Zilch verification program entrypoint");
+
+    // Iterating accounts is safer than indexing
+    let accounts_iter = &mut accounts.iter();
+
+    // Get the account to say hello to
+    let account = next_account_info(accounts_iter)?;
+
+    //load pointers to proof info in account
+    //hardcoded values for test 1.
+
+    //load account where Proof is stored.
+    //let account_key = Pubkey::from_str("7fkcbALYxj5BDq6ZDtWF2hvLMx5K4jTKUrWE4eaxvgoE").unwrap();
+    //let account_info = AccountInfo::from_account(&account, false);
+    //let account_key = Pubkey::decode("7fkcbALYxj5BDq6ZDtWF2hvLMx5K4jTKUrWE4eaxvgoE");
+    let accts = account;
+    //let accts: &AccountInfo = "7fkcbALYxj5BDq6ZDtWF2hvLMx5K4jTKUrWE4eaxvgoE"; //proof deployed on devnet
+    //let raw_proof = bytemuck::try_from_bytes(accts.data.borrow()) //.unwrap();
+    let slice = *accts.data.borrow();
+    let raw_proof = bytemuck::try_from_bytes(&slice).unwrap();
+    let my_proof = StarkProof::from_bytes(raw_proof).unwrap();
+    //let mut my_proof = StkProof::try_from_slic(&accts.data.borrow())?;
+    //let program_hash = "c8653f31a1098e1b83c5d4972ec544cac00aa784bba18b5a9db7478977d38e68";
+    //let program_hash = Element:from_slice("c8653f31a1098e1b83c5d4972ec544cac00aa784bba18b5a9db7478977d38e68");
+    //let program_hash_bytes = hex::from_hex(program_hash_str).unwrap();
+    //let program_hash = ElementDigest::from_slice(&program_hash2);
+    //let program_hash= Digest::from_slice("c8653f31a1098e1b83c5d4972ec544cac00aa784bba18b5a9db7478977d38e68");
+    let inputs: &[u64] = &[1, 1];
+    // The account must be owned by the program in order to modify its data
+    if account.owner != program_id {
+        msg!("Greeted account does not have the correct program id");
+        return Err(ProgramError::IncorrectProgramId);
+    }
+    //let outputs2 = ["8911120806959712300", "11112721240812633725", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"];
+
+    //let outputs: &ProgramOutputs::from_bytes<outputs2>;
+    let stack1 : Vec<u64> = Vec::from([8911120806959712300, 11112721240812633725, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    let ofl1: Vec<u64> = [].to_vec();
+    let outputs = &ProgramOutputs::new(stack1, ofl1);
+    let mut outcome = verify(program_hash, inputs, outputs, my_proof);
+
+    // Increment and store the number of times the account has been greeted
+    //let mut greeting_account = GreetingAccount::try_from_slice(&account.data.borrow())?;
+    //greeting_account.counter += 1;
+    //greeting_account.serialize(&mut &mut account.data.borrow_mut()[..])?;
+
+    msg!("verification complete");
+
+    Ok(())
+}
 
 pub fn verify(
     program_hash: Digest,
